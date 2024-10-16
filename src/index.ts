da5cc05dd7f2fec6165bc5949212f7835808843e
silver <silvi97lila@gmail.com>
2024-10-16 13:16:53 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,111 @@
+import type {
+  ArticleListOutput,
+  AuthorListOutput,
+  BlogOutput,
+  CategoryListOutput,
+  ArticleBySlugInput,
+  ArticleListInput,
+  LocaleOutput,
+  LocalizedArticleOutput,
+} from "./types.js";
+import fetch from "node-fetch";
+
+export default class V0Client {
+  private auth: string;
+  private blogId: string;
+  private domain: string;
+  private version: string;
+
+  constructor(opts: { auth: string; blogId: string }) {
+    if (!opts.auth || !opts.blogId) {
+      throw new Error("Missing required options: auth, domain and blogId");
+    }
+
+    this.auth = opts.auth;
+    this.blogId = opts.blogId;
+    this.version = "v0";
+    this.domain = "https://api.vaporcms.com";
+  }
+
+  private async request(
+    path: string,
+    query: Record<string, any> = {}
+  ): Promise<any> {
+    const url = new URL(
+      `${this.domain}/${this.version}/${this.blogId}/${path}`
+    );
+
+    if (!this.auth) throw new Error("Please provide the API token");
+
+    const headers = new Headers({
+      Authorization: `Bearer ${this.auth}`,
+    });
+
+    Object.entries(query).forEach(([key, value]) => {
+      if (value !== undefined) {
+        if (Array.isArray(value)) {
+          value.forEach((val) =>
+            url.searchParams.append(key, decodeURIComponent(val))
+          );
+        } else {
+          url.searchParams.append(key, String(value));
+        }
+      }
+    });
+
+    try {
+      const response = await fetch(url.toString(), {
+        method: "GET",
+        headers,
+      });
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(
+          `API call failed with status ${response.status}: ${errorText}`
+        );
+      }
+
+      const data = await response.json();
+      return { ok: true, data: data };
+    } catch (error) {
+      if (error instanceof Error) {
+        return { ok: false, error: error.message };
+      }
+
+      console.error(error);
+      return { ok: false, error: "Something went wrong." };
+    }
+  }
+
+  public readonly blog = {
+    get: (): Promise<BlogOutput> => this.request("blog"),
+  };
+
+  public readonly locales = {
+    list: (): Promise<LocaleOutput> => this.request("locales"),
+  };
+
+  public readonly authors = {
+    list: (): Promise<AuthorListOutput> => this.request("authors"),
+  };
+
+  public readonly categories = {
+    list: (): Promise<CategoryListOutput> => this.request("categories"),
+  };
+
+  public readonly article = {
+    get: (args: ArticleBySlugInput): Promise<LocalizedArticleOutput> =>
+      this.request(`articles/${args.slug}`, args),
+
+    list: (args: ArticleListInput): Promise<ArticleListOutput> =>
+      this.request("articles", args),
+  };
+}
+
+// Test it out
+const client = new V0Client({
+  auth: "eyJhbGciOiJIUzI1NiJ9.eyJibG9nSWQiOiJjbTJhZGtwY2UwMDAzMTRwOGtuc3U4bHRsIiwidHlwZSI6IkRyYWZ0IiwiZXhwIjozMTc4OTk0NDAwfQ.VmfNss9PBfdWxCUepYMbFdQ_8WL9BInfzfJXz1FbPeM",
+  blogId: "cm2adkpce000314p8knsu8ltl",
+});
+
+const { data } = await client.blog.get();
---
+++ src/types.ts
@@ -0,0 +1,186 @@
+export type Media = {
+  id: string;
+  url: string;
+  alt: string | null;
+  width: number | null;
+  height: number | null;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type ArticleList = Array<{
+  id: string;
+  status: "Draft" | "Published";
+  createdAt: string;
+  updatedAt: string;
+  localized: Record<
+    string,
+    {
+      id: string;
+      status: "Draft" | "Published";
+      publishedAt: string | null;
+      cover: Media;
+      tableOfContents?: any[];
+      content: {
+        title: string;
+        slug: string;
+        excerpt: string;
+        html: string;
+      };
+      seo: {
+        title: string;
+        description: string;
+        image: Media | null;
+      };
+      authors: Array<{
+        id: string;
+        name: string;
+        slug: string;
+        profile: string | null;
+        createdAt: string;
+        updatedAt: string;
+      }>;
+      categories: Array<{
+        id: string;
+        name: string;
+        slug: string;
+        description: string;
+        createdAt: string;
+        updatedAt: string;
+      }>;
+    }
+  >;
+}>;
+
+export type ApiResult<T> = {
+  ok: boolean;
+  data?: T;
+  error?: string;
+};
+
+// INPUTS
+export type ArticleBySlugInput = {
+  slug: string;
+  localeCode?: string;
+};
+
+export type ArticleListInput = {
+  page?: number;
+  pageSize?: number;
+  categoryIds?: string[];
+  authorIds?: string[];
+};
+
+// OUTPUTS
+export type BlogOutput = ApiResult<{
+  id: string;
+  title: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+}>;
+
+export type LocaleOutput = ApiResult<
+  Array<{
+    id: string;
+    blogId: string;
+    name: string;
+    code: string;
+    default: boolean;
+    createdAt: string;
+    updatedAt: string;
+  }>
+>;
+
+export type ArticleListOutput = ApiResult<{
+  articles: ArticleList;
+  totalCount: number;
+  pagination: {
+    page: number;
+    pageSize: number;
+    totalPages: number;
+  };
+}>;
+
+export type LocalizedArticleOutput = ApiResult<{
+  id: string;
+  status: "Draft" | "Published";
+  publishedAt: string | null;
+  content: {
+    title: string;
+    slug: string;
+    excerpt: string;
+    html: string;
+  };
+  tableOfContents: any[];
+  seo: {
+    title: string;
+    description: string;
+    image: {
+      id: string;
+      url: string;
+      alt: string | null;
+      width: number | null;
+      height: number | null;
+      createdAt: string;
+      updatedAt: string;
+    } | null;
+  };
+  cover: {
+    id: string;
+    url: string;
+    alt: string | null;
+    width: number | null;
+    height: number | null;
+    createdAt: string;
+    updatedAt: string;
+  };
+  categories: Array<{
+    id: string;
+    name: string;
+    slug: string;
+    description: string;
+    createdAt: string;
+    updatedAt: string;
+  }>;
+  authors: Array<{
+    id: string;
+    name: string;
+    slug: string;
+    profile: string;
+    createdAt: string;
+    updatedAt: string;
+  }>;
+  locales: string[];
+  locale: string;
+  createdAt: string;
+  updatedAt: string;
+}>;
+
+export type AuthorListOutput = ApiResult<
+  Array<{
+    id: string;
+    name: string;
+    slug: string;
+    description: Record<string, string>;
+    media: Record<string, Media>;
+    createdAt: string;
+    updatedAt: string;
+  }>
+>;
+
+export type CategoryListOutput = ApiResult<
+  Array<{
+    id: string;
+    localized: Record<
+      string,
+      {
+        name: string;
+        slug: string;
+        description: string;
+      }
+    >;
+    createdAt: string;
+    updatedAt: string;
+  }>
+>;
